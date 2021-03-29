8b84c55f35581e6e0bcdeddf399d21263ccfc44b
srikanthpragada <srikanthpragada@gmail.com>
2021-03-29 19:57:50 +0530

    29-MAR

--- demo/oop/shapes.py
+++ demo/oop/shapes.py
@@ -1,4 +1,8 @@
-class Point:
+from abc import abstractmethod, ABC
+
+
+# abstract class 
+class Point(ABC):
     def __init__(self, x, y):
         if x < 0 or y < 0:
             raise ValueError("Invalid Co-ordinates")
@@ -8,6 +12,10 @@ class Point:
     def __str__(self):
         return f"{self.x},{self.y}"
 
+    @abstractmethod
+    def area(self):
+        pass
+
 
 class Circle(Point):
     def __init__(self, x, y, radius):
@@ -16,6 +24,9 @@ class Circle(Point):
 
     def __str__(self):
         return f"{super().__str__()},{self.radius}"
+
+    def area(self):
+        return 0
 
 
 class Rectangle(Point):
@@ -27,11 +38,14 @@ class Rectangle(Point):
     def __str__(self):
         return f"{super().__str__()},{self.length},{self.width}"
 
+    def area(self):
+        return 0
 
-c = Circle(10,20,30)
+
+c = Circle(10, 20, 30)
 print(c)
-r = Rectangle(5,5,10,20)
+r = Rectangle(5, 5, 10, 20)
 print(r)
 
-print( issubclass(Circle,Point))
-print( issubclass(Circle,Rectangle))
+print(issubclass(Circle, Point))
+print(issubclass(Circle, Rectangle))
